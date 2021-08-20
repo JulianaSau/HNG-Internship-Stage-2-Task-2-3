0b8a3c3201abd7f5b54382dc261d8ef2bed20444
Juliana Sau <49183775+JulianaSau@users.noreply.github.com>
2021-08-20 22:12:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Stage-2-Task-2-3
+ HNG Internship Stage 2 Task 2 & 3. 
---
+++ printname.c
@@ -0,0 +1,11 @@
+/*HNGi8 Internship Stage 2 task 2*/
+/*Compulsory: Write a simple program that prints your name using your language of choice.
+*/
+
+#include <stdio.h>
+
+main() {
+   
+    printf("Juliana Sau"); // Prints out my name
+    return 0;
+}
